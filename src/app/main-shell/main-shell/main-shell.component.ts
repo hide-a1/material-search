67dd41936f10798b64f416472345ef69c220ffc6
hide-a1 <hide210g@gmail.com>
2021-08-04 12:27:50 +0900

    fix(main-shell): 遷移修正

--- src/app/main-shell/main-shell/main-shell.component.ts
+++ src/app/main-shell/main-shell/main-shell.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
@@ -7,11 +8,11 @@ import { AuthService } from 'src/app/services/auth.service';
   styleUrls: ['./main-shell.component.scss'],
 })
 export class MainShellComponent implements OnInit {
-  constructor(public authService: AuthService) {}
+  constructor(public authService: AuthService, private router: Router) {}
 
   ngOnInit(): void {}
 
   logout(): void {
-    this.authService.logout();
+    this.authService.logout().then(() => this.router.navigate(['login']));
   }
 }

5a7062e47bbf0eac5311f354fa94b3d9124a97fe
hide-a1 <hide210g@gmail.com>
2021-08-06 03:50:41 +0900

    feat(home): コンポーネント作成

--- src/app/main-shell/main-shell-routing.module.ts
+++ src/app/main-shell/main-shell-routing.module.ts
@@ -12,6 +12,11 @@ const routes: Routes = [
         loadChildren: () =>
           import('./create/create.module').then((m) => m.CreateModule),
       },
+      {
+        path: '',
+        loadChildren: () =>
+          import('./home/home.module').then((m) => m.HomeModule),
+      },
     ],
   },
 ];
